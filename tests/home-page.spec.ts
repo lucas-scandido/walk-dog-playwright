af3686bbd6c00319834626449fe6bda7b31f0112
luccasdasilvacandido <luccasdasilvacandido@gmail.com>
2025-02-22 01:05:46 -0300

    🧪 [test]: add test to validate home page elements

---
+++ tests/home-page.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Home Page - Walkdog', () => {
+  test('Should be displayed the home page elements', async ({ page }) => {
+    await test.step('Acessar a página inicial', async () => {
+      await page.goto('/');
+    });
+
+    await test.step('Validate if the logo is visible', async () => {
+      const logo = page.getByRole('img', { name: 'Walkdog' });
+      await expect(logo).toBeVisible();
+    });
+
+    await test.step('Validate the title and main texts', async () => {
+      const heading = page.getByRole('heading');
+      const description = page.getByText(
+        'Quer gerar uma renda extra passeando com pets? Faça parte da nossa comunidade de dog walkers.'
+      );
+
+      await expect.soft(heading).toHaveText('Cuidado e diversão em cada passo');
+      await expect.soft(description).toBeVisible();
+    });
+
+    await test.step('Validate if the registration button is visible', async () => {
+      const botaoDogWalker = page.getByRole('link', { name: 'Quero ser Dog Walker' });
+      
+      await expect(botaoDogWalker).toBeVisible();
+    });
+  });
+});
