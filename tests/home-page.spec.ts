9cfacc04106a563c16c3a6163308819fa5f9c3d9
luccasdasilvacandido <luccasdasilvacandido@gmail.com>
2025-02-22 15:33:02 -0300

    🛠️ [refactor]: refactoring home page tests and applying good practices

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -1,31 +1,24 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { Pages } from '../pages/pages';
 
 test.describe('Home Page - Walkdog', () => {
-  test('Should be displayed the home page elements', async ({ page }) => {
+  test('Validate home page elements', async ({ page }) => {
+    const pages = new Pages(page);
+
     await test.step('Access the home page', async () => {
       await page.goto('/');
     });
 
-    await test.step('Validate if the logo is visible', async () => {
-      const logo = page.getByRole('img', { name: 'Walkdog' });
-      
-      await expect(logo).toBeVisible();
+    await test.step('Validate the logo', async () => {
+      await pages.homePage.validateLogo();
     });
 
     await test.step('Validate the title and main texts', async () => {
-      const heading = page.getByRole('heading');
-      const description = page.getByText(
-        'Quer gerar uma renda extra passeando com pets? Faça parte da nossa comunidade de dog walkers.'
-      );
-
-      await expect.soft(heading).toHaveText('Cuidado e diversão em cada passo');
-      await expect.soft(description).toBeVisible();
+      await pages.homePage.validateTitleAndTexts();
     });
 
-    await test.step('Validate if the registration button is visible', async () => {
-      const botaoDogWalker = page.getByRole('link', { name: 'Quero ser Dog Walker' });
-      
-      await expect(botaoDogWalker).toBeVisible();
+    await test.step('Validate the registration button', async () => {
+      await pages.homePage.validateRegistrationButton();
     });
   });
 });
